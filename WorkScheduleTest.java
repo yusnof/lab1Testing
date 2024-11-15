a8731768425094f81eff362f0978e06e11ca6eab
GunnarJohansson <146834192+GunnarJohansson@users.noreply.github.com>
2024-11-15 15:23:46 +0100

    Added WorkScheduleTest and an attempt for a test method. It does not work currently.

---
+++ WorkScheduleTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WorkScheduleTest {
+
+    //TODO Partitions?!?!?!
+    //TODO Understand the methods ;D
+    @Test
+    public void setRequiredNumberTest()  {
+        int size = 5;
+        WorkSchedule ws = new WorkSchedule(10);
+        WorkSchedule.Hour h = ws.readSchedule(0);
+        int nEmployees = h.requiredNumber;
+        String[] employees = h.workingEmployees;
+        ws.setRequiredNumber(1,1,5);
+
+        WorkSchedule.Hour hRes = ws.readSchedule(size);
+        int EmployeeRes = hRes.requiredNumber;
+        String[] employeesRes = hRes.workingEmployees;
+
+        assertEquals(nEmployees, EmployeeRes);
+    }
+}
